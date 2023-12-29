042bef43c86261358f0cdc53b1eda493a05e4e11
Rohan Karanjit <109459345+rohankaranjit@users.noreply.github.com>
2023-12-30 00:03:07 +0545

    Update SD_02_amazonurl.py

--- SD_02_amazonurl.py
+++ SD_02_amazonurl.py
@@ -6,3 +6,8 @@ def scrape_amazon_products(url):
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
     }
+
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+
