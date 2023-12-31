5ad2458bf1f961753e86de743c1a4d029c0daf0f
Rohan Karanjit <109459345+rohankaranjit@users.noreply.github.com>
2023-12-31 23:23:30 +0545

    Update SD_02_amazonurl.py

--- SD_02_amazonurl.py
+++ SD_02_amazonurl.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
+
 def scrape_amazon_products(url):
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
